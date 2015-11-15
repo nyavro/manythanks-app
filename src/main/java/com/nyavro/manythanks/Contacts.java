4e08d40628fff70f25ddd2294c1ba0975d3f498a
Nyavro <e.nyavro@gmail.com>
2015-11-15 23:40:08 +0600

    Phone contacts list retrieve

---
+++ src/main/java/com/nyavro/manythanks/Contacts.java
@@ -0,0 +1,9 @@
+package com.nyavro.manythanks;
+
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+public class Contacts {
+    public static final String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+    public static final Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+}
--- src/main/scala/com/nyavro/manythanks/ContactsView.scala
+++ src/main/scala/com/nyavro/manythanks/ContactsView.scala
@@ -1,7 +1,10 @@
 package com.nyavro.manythanks
 
+import android.database.Cursor
 import android.widget.ArrayAdapter
 import org.scaloid.common._
+
+import scala.annotation.tailrec
 
 class ContactsView extends SActivity {
 
@@ -23,5 +26,23 @@ class ContactsView extends SActivity {
     setContentView(new SVerticalLayout += list)
   }
 
-  def contacts() = List("Would", "be", "wrongful")
+  def contacts() = {
+    val projection = Array(Contacts.DISPLAY_NAME)
+    val cur = getContentResolver.query(Contacts.CONTENT_URI, projection, null, null, null)
+    val index = cur.getColumnIndex(Contacts.DISPLAY_NAME)
+    cur.moveToFirst()
+    toList(cur) {
+      cur => cur.getString(index)
+    }
+  }
+
+  @tailrec
+  private def toList[A](cursor: Cursor, acc:List[A] = Nil)(convert: Cursor => A): List[A] = {
+    if (cursor.isAfterLast) acc
+    else {
+      val item = convert(cursor)
+      cursor.moveToNext
+      toList(cursor, item::acc)(convert)
+    }
+  }
 }
