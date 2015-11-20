aa46dd2fbc7fd59739aa09cd4ee0f1b98651de69
Nyavro <e.nyavro@gmail.com>
2015-11-20 22:27:28 +0600

    Smooth phone list update

--- src/main/java/com/nyavro/manythanks/Contacts.java
+++ src/main/java/com/nyavro/manythanks/Contacts.java
@@ -4,8 +4,11 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 
 public class Contacts {
+    public static final Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
     public static final String DISPLAY_NAME = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
     public static final String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
     public static final String ID = ContactsContract.Contacts._ID;
-    public static final Uri CONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    public static final Uri PHONE_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    public static final String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+    public static final String PHONE_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
 }
--- src/main/scala/com/nyavro/manythanks/ContactsView.scala
+++ src/main/scala/com/nyavro/manythanks/ContactsView.scala
@@ -3,6 +3,10 @@ package com.nyavro.manythanks
 import android.database.Cursor
 import android.widget.ArrayAdapter
 import org.scaloid.common._
+import rx.android.schedulers.AndroidSchedulers
+import rx.lang.scala.JavaConversions._
+import rx.lang.scala.{Observable, Observer, Subscription}
+import rx.schedulers.Schedulers
 
 import scala.annotation.tailrec
 
@@ -14,31 +18,47 @@ class ContactsView extends SActivity {
 
   lazy val list = new SListView
 
+  lazy val adapter = new ArrayAdapter[String](
+    this,
+    android.R.layout.simple_list_item_1
+  )
 
   onCreate {
-    list.setAdapter(
-      new ArrayAdapter[String](
-        this,
-        android.R.layout.simple_list_item_1,
-        contacts().toArray
-      )
-    )
+    list.setAdapter(adapter)
     setContentView(new SVerticalLayout += list)
+    adapter.setNotifyOnChange(true)
+    Observable.create {
+      (observer: Observer[(String, List[String])]) => {
+        contacts().foreach {
+          case (id, name) => observer.onNext(name, phones(id))
+        }
+        Subscription()
+      }
+    }
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribeOn(Schedulers.io())
+      .subscribe(
+        res => {
+          adapter.add(res._1 + res._2.mkString(";"))
+        }
+      )
   }
 
   def contacts() = {
-    val projection = Array(Contacts.DISPLAY_NAME, Contacts.NUMBER)
     val cur = getContentResolver.query(Contacts.CONTENT_URI, null, null, null, null)
     val nameIndex = cur.getColumnIndex(Contacts.DISPLAY_NAME)
-    val idIndex = cur.getColumnIndex(Contacts.NUMBER)
-
+    val idIndex = cur.getColumnIndex(Contacts.ID)
+    val hasPhoneIndex = cur.getColumnIndex(Contacts.HAS_PHONE_NUMBER)
     toList(cur) {
-      cur => {
-        cur.getString(nameIndex) + " " + cur.getString(idIndex)
-      }
-    }
+      cur => if(cur.getString(hasPhoneIndex).toInt > 0) Some((cur.getString(idIndex), cur.getString(nameIndex))) else None
+    }.flatten
   }
 
+  def phones(contactId:String) = {
+    val cur = getContentResolver.query(Contacts.PHONE_URI, null, s"${Contacts.PHONE_ID} = ?", Array(contactId), null)
+    val phoneIndex = cur.getColumnIndex(Contacts.NUMBER)
+    toList(cur) { cur => cur.getString(phoneIndex) }
+  }
 
   private def toList[A](cursor: Cursor)(convert: Cursor => A): List[A] = {
     @tailrec
